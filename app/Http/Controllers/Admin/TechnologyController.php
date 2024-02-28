7c6d71ad9f1a8f4ff44b6f010e9bcd9170e00014
xJustFox <dodydaversa@gmail.com>
2024-02-28 16:40:49 +0100

    [Update] Added delete technology

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -118,6 +118,8 @@ class TechnologyController extends Controller
      */
     public function destroy(Technology $technology)
     {
-        //
+        $technology->delete();
+
+        return redirect()->route('admin.technologies.index');
     }
 }
